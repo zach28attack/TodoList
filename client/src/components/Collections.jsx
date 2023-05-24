887abe69c45a56289b2336b4bba5534bbf7b1070
Zachary Casares <rrumble250@gmail.com>
2023-05-24 17:31:31 -0400

    change title of collections

--- client/src/components/Collections.jsx
+++ client/src/components/Collections.jsx
@@ -5,7 +5,7 @@ function Collections() {
   let collections = ["Work", "Home", "Hobby", "Health"];
   return (
     <div className={Class.border}>
-      <h1 className={Class.header}>Collections</h1>
+      <h1 className={Class.header}>Lists</h1>
       {collections.map((item) => (
         <Collection title={item} key={item} />
       ))}
