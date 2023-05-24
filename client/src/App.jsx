d460a1d84e85dcb83b62755c641b9a2da79b2ad5
Zachary Casares <rrumble250@gmail.com>
2023-05-24 17:38:46 -0400

    move files into specefic folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import app from "./App.module.css";
-import Collections from "./components/Collections";
-import List from "./components/List";
+import Collections from "./components/collection/Collections.jsx";
+import List from "./components/list/List.jsx";
 function App() {
   return (
     <div className={app.grid}>
