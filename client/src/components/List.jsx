32174374b068d708e218a9786487d026d0d73471
Zachary Casares <rrumble250@gmail.com>
2023-05-24 16:26:13 -0400

    import list header and list item components

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,9 +1,17 @@
 import Class from "./List.module.css";
 import ListHeader from "./ListHeader.jsx";
+import ListItem from "./ListItem.jsx";
+
 function List() {
+  const items = ["Mow lawn", "Wash dishes", "Do taxes", "Fix doorhinge"];
   return (
     <>
-      <ListHeader />
+      <div>
+        <ListHeader />
+        {items.map((item) => (
+          <ListItem title={item} />
+        ))}
+      </div>
     </>
   );
 }
