88459906216c5be69db2b180498e9fb1cd4a9bd9
Zachary Casares <rrumble250@gmail.com>
2023-05-24 19:31:18 -0400

    use createPortal to render form

--- client/src/components/list/ListHeader.jsx
+++ client/src/components/list/ListHeader.jsx
@@ -1,15 +1,24 @@
 import Class from "./ListHeader.module.css";
 import {RiAccountCircleLine, RiAddCircleLine} from "react-icons/ri";
+import ListForm from "../utility/ListForm.jsx";
+import {createPortal} from "react-dom";
+import {useState} from "react";
 
 function ListHeader() {
+  const [showForm, setShowForm] = useState(false);
+  const renderFormHandler = () => {
+    setShowForm(true);
+  };
+
   return (
     <div className={Class.header}>
       <button className={Class.button}>
         <RiAccountCircleLine />
       </button>
 
-      <button className={Class.button}>
+      <button className={Class.button} onClick={renderFormHandler}>
         <RiAddCircleLine />
+        {showForm && createPortal(<ListForm />, document.getElementById("list-form-root"))}
       </button>
     </div>
   );
