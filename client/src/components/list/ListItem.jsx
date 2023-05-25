3aaa35e3b967ccfab17113b6e8f2081265fb982d
Zachary Casares <rrumble250@gmail.com>
2023-05-25 17:25:04 -0400

    add check box function

--- client/src/components/list/ListItem.jsx
+++ client/src/components/list/ListItem.jsx
@@ -1,16 +1,23 @@
 import Class from "./ListItem.module.css";
 import {RiCheckboxBlankCircleLine, RiCheckboxBlankCircleFill, RiPencilFill, RiDeleteBin6Line} from "react-icons/ri";
+import {useState} from "react";
 
 function ListItem(props) {
   const onDeleteItem = () => {
     props.onDeleteItem(props.id);
   };
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const checkboxHandler = () => {
+    setIsCompleted(!isCompleted);
+  };
+
   return (
     <div className={Class.item}>
       <div className={Class.titleGroup}>
         {!props.isForm && (
-          <div className={Class.circle}>
-            <RiCheckboxBlankCircleLine />
+          <div className={Class.checkbox} onClick={checkboxHandler}>
+            {isCompleted ? <RiCheckboxBlankCircleFill /> : <RiCheckboxBlankCircleLine />}
           </div>
         )}
         <div className={Class.title}>{props.children}</div>
