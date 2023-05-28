855a8a17d97e0d144f2cc2fa29096e33983d518e
Zachary Casares <rrumble250@gmail.com>
2023-05-28 15:45:16 -0400

    adjust background color

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,19 +1,18 @@
 import ListHeader from "./ListHeader.jsx";
 import ListItem from "./ListItem.jsx";
+import Class from "./List.module.css";
 
 function List(props) {
   return (
-    <>
-      <div>
-        <ListHeader submitHandler={props.submitHandler} />
-        <div id="list-form-root"></div>
-        {props.items.map((item) => (
-          <ListItem key={item} onDeleteItem={props.onDeleteItem} id={item}>
-            {item}
-          </ListItem> //update key with id
-        ))}
-      </div>
-    </>
+    <div className={Class.backgroundColor}>
+      <ListHeader submitHandler={props.submitHandler} />
+      <div id="list-form-root"></div>
+      {props.items.map((item) => (
+        <ListItem key={item} onDeleteItem={props.onDeleteItem} id={item}>
+          {item}
+        </ListItem> //update key with id
+      ))}
+    </div>
   );
 }
 
