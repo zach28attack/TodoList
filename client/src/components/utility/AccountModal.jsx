ac7ab5c00be82e91d4d12e77fa26307875aa843d
Zachary Casares <rrumble250@gmail.com>
2023-05-28 15:58:01 -0400

    add form to modal

--- client/src/components/utility/AccountModal.jsx
+++ client/src/components/utility/AccountModal.jsx
@@ -1,15 +1,36 @@
 import Class from "./AccountModal.module.css";
 
+const Form = (props) => {
+  return (
+    <div>
+      <button className={Class.closeBtn} onClick={props.removeModalHandler}>
+        X
+      </button>
+      <form action="">
+        <label>Email</label>
+        <input type="text" name="email"></input>
+        <label>Password</label>
+        <input type="text" name="password"></input>
+        <label>Password-confirmation</label>
+        <input type="text" name="password-confirmation"></input>
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
 function AccountModal(props) {
   const removeModalHandler = (e) => {
-    if (e.target.classList.contains(Class.modalBackground)) {
+    if (e.target.classList.contains(Class.modalBackground) || e.target.classList.contains(Class.closeBtn)) {
       props.removeModal();
     }
   };
 
   return (
     <div className={Class.modalBackground} onClick={removeModalHandler}>
-      <div className={Class.modalContent}>{props.children}</div>
+      <div className={Class.modalContent}>
+        <Form removeModal={removeModalHandler} />
+      </div>
     </div>
   );
 }
