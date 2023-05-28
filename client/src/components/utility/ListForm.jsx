2b6d48a735a440b2f4872cc6cd4c5f806afa920e
Zachary Casares <rrumble250@gmail.com>
2023-05-28 19:54:32 -0400

    change format of form

--- client/src/components/utility/ListForm.jsx
+++ client/src/components/utility/ListForm.jsx
@@ -9,10 +9,10 @@ function ListForm(props) {
   return (
     <ListItem isForm="true">
       <form className={Class.form} onSubmit={submitHandler}>
-        <input name="item" type="text" placeholder="What to-do?" className={Class.input}></input>
         <button className={Class.button}>
           <RiAddFill />
         </button>
+        <input name="item" type="text" placeholder="What to-do?" className={Class.input}></input>
       </form>
     </ListItem>
   );
