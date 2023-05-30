b8e4dff8aa66f57ad03675a42f744818dcf98816
Zachary Casares <rrumble250@gmail.com>
2023-05-30 13:04:15 -0400

    add delete function

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -28,3 +28,12 @@ export async function deleteAll() {
     console.error("error caught:", error);
   }
 }
+export async function deleteById(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/collections/delete${id}`);
+    const data = await response.json();
+    console.log("successful MSG:", data.message);
+  } catch (error) {
+    console.error("error caught:", error);
+  }
+}
