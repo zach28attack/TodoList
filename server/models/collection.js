b738b6cdfc8a95beb41e9ed3f278983920a2ac2a
Zachary Casares <rrumble250@gmail.com>
2023-05-30 13:19:58 -0400

    add database functions

--- server/models/collection.js
+++ server/models/collection.js
@@ -1,18 +1,63 @@
 const database = require("../database.js");
+const mongoDB = require("mongodb");
 
 class Collection {
-  constructor(name, isCompleted) {
+  constructor(id, name, itemName, itemIsCompleted) {
+    this.id = id;
     this.name = name;
-    this.isCompleted = isCompleted;
+    this.itemIsCompleted = itemIsCompleted;
+    this.itemName = itemName;
   }
-  async save() {
-    const db = database.connectToDatabase();
+  async saveCollection() {
+    try {
+      const db = await database.connectToDatabase();
+      db.collection("collections").insertOne({name: this.name, items: [{name: "", itemIsCompleted: undefined}]});
+    } catch (error) {
+      console.error("Error caught", error);
+    }
+  }
 
-    db.collection("collections").insertOne({name: this.name, isCompleted: this.isCompleted});
-    console.log("saved");
+  async saveItem() {
+    try {
+      const db = await database.connectToDatabase();
+      db.collection("collections").updateOne(
+        {
+          _id: new mongoDB.ObjectId(this.id),
+        },
+        {
+          $push: {items: {name: this.itemName, isCompleted: this.itemIsCompleted}},
+        }
+      );
+    } catch (error) {
+      console.error("Error caught", error);
+    }
   }
-  static fetchAll() {
+  async deleteCollectionById() {
+    try {
+      const db = await database.connectToDatabase();
+      db.collection("collections").deleteOne({_id: new mongoDB.ObjectId(this.id)});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async deleteItemById() {
+    try {
+      const db = await database.connectToDatabase();
+      db.collection("collections")
+        .findOne({_id: new mongoDB.ObjectId(this.id)})
+        .updateOne({_id: new mongoDB.ObjectId(this.id)}, {$pull: {items: {name: this.name}}});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async fetchAll() {
+    const db = await database.connectToDatabase();
     return db.collection("collections").find().toArray();
+  }
+  async deleteAll() {
+    const db = await database.connectToDatabase();
+    db.collection("collections").deleteMany({});
   }
 }
 module.exports = Collection;
