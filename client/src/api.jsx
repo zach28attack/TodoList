c40c13491342f80b4ca1f667a60076c811e70a4b
Zachary Casares <rrumble250@gmail.com>
2023-05-30 13:04:03 -0400

    move api code into seperate file

---
+++ client/src/api.jsx
@@ -0,0 +1,30 @@
+export async function deleteAll() {
+  try {
+    const response = await fetch("http://localhost:3000/collections/delete-all");
+    const data = await response.json();
+    console.log("successful MSG:", data.message);
+  } catch (error) {
+    console.error("error caught:", error);
+  }
+}
+
+export async function fetchAll() {
+  try {
+    const response = await fetch("http://localhost:3000/collections");
+    const data = await response.json();
+    console.log("successful MSG:", data.message);
+    return data;
+  } catch (error) {
+    console.error("error caught:", error);
+  }
+}
+
+export async function deleteAll() {
+  try {
+    const response = await fetch("http://localhost:3000/collections/delete-all");
+    const data = await response.json();
+    console.log("successful MSG:", data.message);
+  } catch (error) {
+    console.error("error caught:", error);
+  }
+}
