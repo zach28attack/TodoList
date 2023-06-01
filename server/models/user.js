17327bc5a784b437371e1ca0bf9ba051fe59c86f
Zachary Casares <rrumble250@gmail.com>
2023-06-01 19:07:07 -0400

    add delete user route

--- server/models/user.js
+++ server/models/user.js
@@ -2,16 +2,24 @@ const database = require("../database.js");
 const mongoDB = require("mongodb");
 
 class User {
-  constructor(email, password, passwordConfirmation) {
+  constructor(email, password, id) {
     this.email = email;
     this.password = password;
-    this.passwordConfirmation = passwordConfirmation;
+    this._id = id;
   }
   async saveNewUser() {
     const db = await database.connectToDatabase();
     const result = await db.collection("users").insertOne({email: this.email, password: this.password});
-    console.log("USER:", result);
-    return result.insertedId.toString();
+    this._id = result.insertedId.toString();
+    return this;
+  }
+  async deleteUser() {
+    const db = await database.connectToDatabase();
+    const result = await db.collection("users").deleteOne({_id: new mongoDB.ObjectId(this._id)});
+  }
+  async DeleteAllUsers() {
+    const db = await database.connectToDatabase();
+    const result = await db.collection("users").deleteMany();
   }
 }
 
