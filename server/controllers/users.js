b3c35c9e61865c5972aee832477a805e87dbdc47
Zachary Casares <rrumble250@gmail.com>
2023-06-01 19:07:44 -0400

    add delete function

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,12 +1,13 @@
 const User = require("../models/user.js");
 
-exports.saveNewUser = async (req, res, next) => {
-  const data = req.body;
+exports.signup = async (req, res, next) => {
+  const {email, password, passwordConfirmation} = req.body;
   const user = new User();
-  user.email = data.email;
-  user.password = data.password;
-  if (user.password === data.passwordConfirmation) {
-    user.saveNewUser();
+  user.email = email;
+  user.password = password;
+  if (user.password === passwordConfirmation) {
+    const newUser = await user.saveNewUser();
+    req.session.user = newUser;
     res.status(200).json({
       message: "new user saved",
     });
@@ -15,4 +16,27 @@ exports.saveNewUser = async (req, res, next) => {
       message: "User not saved",
     });
   }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  const user = new User();
+  user.id = req.body.userId;
+  user.deleteUser;
+};
+
+exports.login = async (req, res, next) => {
+  const user = new User();
+  const {email, password} = req.body;
+  user.email = email;
+  user.password = password;
+  if (user.email && user.password) {
+    user.login();
+  }
+};
+
+exports.logout = async (req, res, next) => {};
+
+exports.deleteAll = async (req, res, next) => {
+  const user = new User();
+  user.DeleteAllUsers();
 };
