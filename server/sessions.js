4d7856e838231e21ac73e4ce1cc996e175dfeda8
Zachary Casares <rrumble250@gmail.com>
2023-06-01 14:53:50 -0400

    create session config module

---
+++ server/sessions.js
@@ -0,0 +1,19 @@
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const configureSessions = () => {
+  const store = new MongoDBStore({
+    uri: "mongodb+srv://zach28attack:MongoDBPassword@cluster0.im0uft8.mongodb.net/?retryWrites=true&w=majority",
+    collection: "sessions",
+  });
+
+  app.use(
+    session({
+      secret: "55070",
+      resave: false,
+      saveUninitialized: false,
+      store: store,
+    })
+  );
+};
+
+exports.configureSessions = configureSessions;
