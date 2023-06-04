57e3bd9c6caa0ad9f465de7297d312a15cb31a2c
Zachary Casares <rrumble250@gmail.com>
2023-06-04 14:52:21 -0400

    misc

--- server/utility/authenticateUser.js
+++ server/utility/authenticateUser.js
@@ -6,30 +6,28 @@ module.exports = async (req, res, next) => {
   // extract token from auth header
   let decodedToken;
   // initialize var
-
   try {
-    decodedToken = jwt.verify(token, "newKey");
-    // decodes token, if invalid token it will throw error
-    const db = await database.connectToDatabase();
-    const result = await db.collection("tokens").findOne({userId: decodedToken.id, token: token, revoked: false});
-    // check if token is valid by 'revoked:false' status
-    if (result) {
-      // if token isn't revoked then set req data
-      req.userId = decodedToken.id;
-      req.token = token;
-      next();
-    } else {
-      const error = new Error("Token is expired");
-      error.statusCode = 400;
-      throw error;
+    if (token) {
+      decodedToken = jwt.verify(token, "newKey");
+      // decodes token, if invalid token it will throw error
+      const db = await database.connectToDatabase();
+      const result = await db.collection("tokens").findOne({userId: decodedToken.id, token: token, revoked: false});
+      // check if token is valid by 'revoked:false' status
+      if (result) {
+        // if token isn't revoked then set req data
+        req.userId = decodedToken.id;
+        req.token = token;
+        next();
+      } else {
+        const error = new Error("Token is expired");
+        error.statusCode = 400;
+      }
     }
   } catch (error) {
     error.statusCode = 500;
-    throw error;
   }
   if (!decodedToken) {
     const error = new Error("Not Authenticated");
     error.statusCode = 401;
-    throw error;
   }
 };
